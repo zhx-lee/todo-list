a14e98fe661bdd5444760e34828a426f83cbc69e
zhx-lee <q2484930577@gmail.com>
2026-03-20 00:36:02 +0000

    feat: implement checkbox and strike-through functionality

--- todo.js
+++ todo.js
@@ -13,22 +13,47 @@ function todoMain() {
         inputElem.addEventListener("change", onChange, false);
     }
     function onChange(event) {
+        let flag = true;
+
         let inputValue = inputElem.value;
         inputElem.value = "";
         let liElem = document.createElement("li");
-        liElem.innerText = inputValue;
+
+        let checkboxElem = document.createElement("input");
+        checkboxElem.type = "checkbox";
+        liElem.appendChild(checkboxElem)
+
+        let textElem = document.createElement("span");
+        textElem.innerText = inputValue;
+        liElem.appendChild(textElem);
+
+        // liElem.innerText = inputValue;
+        // liElem.addEventListener("click", onClick, false);
+
         let spanElem = document.createElement("span");
         spanElem.innerText = "delete";
         spanElem.className = "material-symbols-outlined";
 
-
-
         spanElem.addEventListener("click", deleteItem, false);
 
         liElem.appendChild(spanElem);
+
         ulElem.appendChild(liElem);
         function deleteItem() {
             liElem.remove();
+        }
+
+        function onClick() {
+            if (flag) {
+                //     this.style.textDecoration = "line-through";
+                //     flag = !flag;
+                this.classList.add("strike");
+            } else {
+                // this.style.textDecoration = "none";
+                // flag = !flag;
+                this.classList.remove("strike");
+            }
+
         }
     }
 }
