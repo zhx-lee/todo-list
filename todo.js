00fb6711d902b4749d72c71e87ab2d8c14bec341
zhx-lee <q2484930577@gmail.com>
2026-03-24 23:35:03 +0000

    feat: implement category filter

--- todo.js
+++ todo.js
@@ -3,7 +3,7 @@ function todoMain() {
     let inputElem,
         inputElem2,
         button,
-        ulElem;
+        selectElem;
     getElements();
     addListeners();
 
@@ -11,11 +11,12 @@ function todoMain() {
         inputElem = document.getElementsByTagName("input")[0];
         inputElem2 = document.getElementsByTagName("input")[1];
         button = document.getElementById("addBtn");
-        ulElem = document.getElementsByTagName("ul")[0]
+        selectElem = document.getElementById("categoryFilter");
 
     }
     function addListeners() {
         button.addEventListener("click", addEntry, false);
+        selectElem.addEventListener("change", filterEntries, false);
     }
     function addEntry(event) {
         let inputValue = inputElem.value;
@@ -59,6 +60,44 @@ function todoMain() {
 
         function done() {
             trElem.classList.toggle("strike");
+        }
+    }
+    function filterEntries() {
+
+        let selection = selectElem.value;
+
+        if (selection == "all") {
+            let rows = document.getElementsByTagName("tr");
+            Array.from(rows).forEach((row, index, array) => {
+                row.style.display = "";
+            });
+
+
+
+        } else {
+            let rows = document.getElementsByTagName("tr");
+
+            Array.from(rows).forEach((row, index, array) => {
+                if (index == 0) {
+                    return;
+                }
+                let category = row.getElementsByTagName("td")[2].innerText;
+                if (category == selectElem.value) {
+                    row.style.display = "";
+                } else {
+                    row.style.display = "none";
+                }
+            });
+
+
+            // for (let i = 1; i < rows.length; i++) {
+            //     let category = rows[i].getElementsByTagName("td")[2].innerText;
+            //     if (category == selectElem.value) {
+            //         rows[i].style.display = "";
+            //     } else {
+            //         rows[i].style.display = "none";
+            //     }
+            // }
         }
     }
 }
